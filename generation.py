73bc24a9400167deeee1987b4830ae2b243350fd
Andrew Walsh <awalsh128@gmail.com>
2012-11-08 21:26:38 -0800

    Added delimiter specification to constructor.

--- generation.py
+++ generation.py
@@ -5,11 +5,12 @@ class MarkovChains:
 
     START_STATE = 0
 
-    def __init__(self):
-        self.cardinalities = dict() # cardinality of each state's sample space        
-        self.transitions = dict()   # state transitions
+    def __init__(self, delimiters):
+        self.delimiters = delimiters    # delimits sentences
+        self.cardinalities = dict()     # cardinality of each state's sample space        
+        self.occurrences = dict()       # state occurrences
         self.cardinalities[MarkovChains.START_STATE] = 0
-        self.transitions[MarkovChains.START_STATE] = dict()
+        self.occurrences[MarkovChains.START_STATE] = dict()
         
     def _add_chain(self, chain):
         states = chain.split()
@@ -20,14 +21,14 @@ class MarkovChains:
             
             for state in states:
                 
-                if prevState in self.transitions:
-                    if state in self.transitions[prevState]:
-                        self.transitions[prevState][state] += 1
+                if prevState in self.occurrences:
+                    if state in self.occurrences[prevState]:
+                        self.occurrences[prevState][state] += 1
                     else:
-                        self.transitions[prevState][state] = 1
+                        self.occurrences[prevState][state] = 1
                 else:
-                    self.transitions[prevState] = dict()
-                    self.transitions[prevState][state] = 1
+                    self.occurrences[prevState] = dict()
+                    self.occurrences[prevState][state] = 1
                         
                 prevState = state
                     
@@ -37,13 +38,13 @@ class MarkovChains:
                     self.cardinalities[state] = 1    
 
     def add_text(self, text):        
-        for chain in re.split(r"[.!?]+", text):
+        for chain in re.split(r"[" + self.delimiters + "]+", text):
             self._add_chain(chain)
 
     def generate_text(self, sentenceCount):
         text = ""
         for i in range(0, sentenceCount):
-            text += " ".join(self._generate_text_sentence()) + ". "
+            text += " ".join(self._generate_text_sentence()) + ".\n"
         return text        
 
     def _generate_text_sentence(self):
@@ -51,15 +52,15 @@ class MarkovChains:
         state = self._get_random_state(MarkovChains.START_STATE)
         while not state is None:            
             sentence.append(state)
-            if state in self.transitions:
+            if state in self.occurrences:
                 state = self._get_random_state(state)
             else:
                 state = None
         return sentence
 
     def _get_random_state(self, state):        
-        states = list(self.transitions[state].keys())
-        cardinalities = list(self.transitions[state].values())
+        states = list(self.occurrences[state].keys())
+        cardinalities = list(self.occurrences[state].values())
         if len(states) > 0:
             mix = []
             for i in range(0, len(states)):
@@ -72,12 +73,12 @@ class MarkovChains:
             return None
 
     def print_chains(self):
-        for tail, headCardinalityPairs in self.transitions.items():        
-            for head, cardinality in headCardinalityPairs.items():
+        for tail, pairs in self.occurrences.items():        
+            for head, occurrence in pairs.items():
                 if tail == MarkovChains.START_STATE:
-                    print("[Start] -> {0} ({1}/{2})".format(head, cardinality, self.cardinalities[tail]))
+                    print("[Start] -> {0} ({1}/{2})".format(head, occurrence, self.cardinalities[tail]))
                 else:
-                    print("{0} -> {1} ({2}/{3})".format(tail, head, cardinality, self.cardinalities[tail]))                  
+                    print("{0} -> {1} ({2}/{3})".format(tail, head, occurrence, self.cardinalities[tail]))                  
 
     
     
--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import re
 file = open("sonnets.txt")
 text = re.sub(r'\n.+\n', '', file.read())
 
-markov = MarkovChains()
+markov = MarkovChains(";:.!?")
 markov.add_text(text)
-print(markov.generate_text(2))
+print(markov.generate_text(4))
